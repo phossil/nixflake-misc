cbbd7d295e72faa943866188d80b3c790acdc3fa
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-03-18 17:10:18 -0400

    pkgs/wapanel: attempt fixes for XDG paths

--- pkgs/wapanel/default.nix
+++ pkgs/wapanel/default.nix
@@ -33,6 +33,18 @@ stdenv.mkDerivation rec {
     gtk3
   ];
 
+  postPatch = ''
+    # Fix XDG paths
+    substituteInPlace thirdparty/xdgpp/xdg.hpp \
+      --replace "/usr/local/share\", \"/usr/share" "/run/current-system/sw/share" \
+      --replace "/etc/xdg" "/run/current-system/sw/etc/xdg"
+
+    # Fix XDG paths in task switcher
+    substituteInPlace src/applets/task-switcher/task_switcher.cc \
+      --replace "/usr/share/applications/\", \"/usr/local/share/applications/" \
+      "/run/current-system/sw/share/applications/"
+  '';
+
   meta = with lib; {
     description =
       "Simple panel/status bar/task bar for your custom stacking Wayland-based desktop";
