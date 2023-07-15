bbde030fa63b55ba7573cef8e81b29b67397e083
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-07-14 20:08:23 -0400

    separate `gnustep-xcode` dependants

--- packages/default.nix
+++ packages/default.nix
@@ -21,19 +21,11 @@ lib.makeScope pkgs.newScope (self: with self; {
   # broken bc im stupid
   lem = callPackage ./lem { };
   wapanel = callPackage ./wapanel { };
-  # broken
-  nimble-commander = callPackage ./nimble-commander {
-    gnustep-xcode = self.xcode;
-  };
   xcode = callPackage ./gnustep/libs/xcode { };
   terminal = callPackage ./gnustep/terminal { };
   price = callPackage ./gnustep/price { };
   step_sync = callPackage ./gnustep/stepsync { };
   themes-gtk = callPackage ./gnustep/plugins/themes-gtk { };
-  # broken
-  skim-app = callPackage ./skim-app {
-    gnustep-xcode = self.xcode;
-  };
   # broken
   cenon = callPackage ./gnustep/cenon { };
   help_viewer = callPackage ./gnustep/helpviewer { };
@@ -45,14 +37,6 @@ lib.makeScope pkgs.newScope (self: with self; {
   steptalk = callPackage ./gnustep/steptalk { };
   text_edit = callPackage ./gnustep/textedit { };
   # broken
-  hex_fiend = callPackage ./hexfiend {
-    gnustep-xcode = self.xcode;
-  };
-  # broken
-  adium = callPackage ./adium {
-    gnustep-xcode = self.xcode;
-  };
-  # broken
   corebase = callPackage ./gnustep/libs/corebase { };
   dbuskit = callPackage ./gnustep/libs/dbuskit { };
   # broken
@@ -62,12 +46,26 @@ lib.makeScope pkgs.newScope (self: with self; {
     simplewebkit = self.simplewebkit;
   };
   # broken
-  filer = callPackage ./filer {
-    gnustep-xcode = self.xcode;
-  };
-  # broken
   cocotron = callPackage ./cocotron { };
   # broken
   themes-win_ux_theme = callPackage ./gnustep/plugins/themes-winuxtheme { };
   cpdup = callPackage ./cpdup { };
+
+  # all broken bc macos projects with gnustep-xcode
+  nimble-commander = callPackage ./nimble-commander {
+    gnustep-xcode = self.xcode;
+  };
+  skim-app = callPackage ./skim-app {
+    gnustep-xcode = self.xcode;
+  };
+  hex_fiend = callPackage ./hexfiend {
+    gnustep-xcode = self.xcode;
+  };
+  # broken
+  adium = callPackage ./adium {
+    gnustep-xcode = self.xcode;
+  };
+  filer = callPackage ./filer {
+    gnustep-xcode = self.xcode;
+  };
 })
