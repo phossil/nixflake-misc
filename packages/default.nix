ef4fab30f08cf42d5427a3d66e769f53994fbe10
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-11-10 10:49:55 -0500

    egmde: purge

--- packages/default.nix
+++ packages/default.nix
@@ -3,7 +3,6 @@
 lib.makeScope pkgs.newScope (self: with self; {
   loudgain = callPackage ./loudgain { };
   qvwm = callPackage ./qvwm { };
-  egmde = callPackage ./egmde { };
   sfwbar = callPackage ./sfwbar { };
   wapanel = callPackage ./wapanel { };
 })
