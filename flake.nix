5c50af6dda26c0d7c7639fa4153941d385f2b03e
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2024-01-07 16:35:35 -0500

    all: purge 2 - electric boogaloo

--- flake.nix
+++ flake.nix
@@ -2,22 +2,23 @@
   description = "flake for testing miscellaneous derivations";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils/v1.0.0;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       lib = nixpkgs.lib;
-
-      # check the packages directory to see
-      # what is available
-      all-packages = import ./packages { inherit pkgs lib; };
     in
     {
       # these are yet to be added to nixpkgs
-      packages.${system} = flake-utils.lib.flattenTree all-packages;
+      packages.${system} = with pkgs; {
+        qvwm = callPackage ./packages/qvwm { };
+        wapanel = callPackage ./packages/wapanel { };
+        unifraktur-cook = callPackage ./packages/unifraktur-cook { };
+        unifraktur-maguntia = callPackage ./packages/unifraktur-maguntia { };
+        brass_mono = callPackage ./packages/brass-mono { };
+      };
 
       # make the flake look pretty :)
       formatter.${system} = pkgs.nixpkgs-fmt;
