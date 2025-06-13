33bedfb9917d9e4338a05655b95eb243c513603e
phossil <38191182+phossil@users.noreply.github.com>
2025-06-12 23:27:23 -0400

    concord : 2.3.0 -> 2.4.0

--- packages/concord/default.nix
+++ packages/concord/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "concord";
-  version = "2.3.0";
+  version = "2.4.0";
 
   src = fetchFromGitHub {
     owner = "Cogmasters";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-u++rb/VaWkGiXUhFddnX74+f3nOtyXWK7+NPsJAJ6ic=";
+    hash = "sha256-OXR7+F77DAGzT8Sy/s2ayuIvgLLYzc7nD5xmDtiBH/0=";
     fetchSubmodules = true;
   };
 
