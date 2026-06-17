a28b64b341c0d92a3ca21e407221d8f9692388c9
phossil <38191182+phossil@users.noreply.github.com>
2026-06-17 19:00:11 -0400

    concord : 2.4.0 -> 2.4.0-unstable-2026-05-02

--- packages/concord/default.nix
+++ packages/concord/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "concord";
-  version = "2.4.0";
+  version = "2.4.0-unstable-2026-05-02";
 
   src = fetchFromGitHub {
     owner = "Cogmasters";
     repo = finalAttrs.pname;
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-OXR7+F77DAGzT8Sy/s2ayuIvgLLYzc7nD5xmDtiBH/0=";
+    rev = "ff3d471d8612447e64b688b07e628ed6df4590a7";
+    hash = "sha256-bR91MdszuJbFqScCwVZSc2bZMEpvlx7kUFTztGjcFaY=";
     fetchSubmodules = true;
   };
 
@@ -22,6 +22,7 @@ stdenv.mkDerivation (finalAttrs: {
   makeFlags = [
     # prefix path defaults to /usr/local
     "PREFIX=${builtins.placeholder "out"}"
+    "SHAREDIR=${builtins.placeholder "out"}/share"
     # defaults to static library instead of shared
     "shared"
   ];
